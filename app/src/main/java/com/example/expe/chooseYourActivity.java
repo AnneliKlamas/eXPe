c38d659286160e6698d175fdc831605cd989e0d4
Anneli Klamas <annelik98@gmail.com>
2020-02-05 15:34:00 +0200

    Alustasin tegevuste valimise haruga

---
+++ app/src/main/java/com/example/expe/chooseYourActivity.java
@@ -0,0 +1,13 @@
+package com.example.expe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class chooseYourActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_your_activity);
+    }
+}
--- app/src/main/java/com/example/expe/findActivity.java
+++ app/src/main/java/com/example/expe/findActivity.java
@@ -1,6 +1,8 @@
 package com.example.expe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class findActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_activity);
+    }
+
+    public void chooseActivity (View view){
+        Intent intent = new Intent(this, chooseYourActivity.class);
+        startActivity(intent);
     }
 }
