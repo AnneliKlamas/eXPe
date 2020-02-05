bedfb8b7c04d57e7cf24a457ee0cd85b5da86785
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2020-02-05 18:31:14 +0200

    profiili alt vajutades complete, tuleb xp juurde. töötab kui andmebaas

--- app/src/main/java/com/example/expe/MainActivity.java
+++ app/src/main/java/com/example/expe/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.expe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +22,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void activity (View view){
+    public void activity(View view) {
         Intent intent = new Intent(this, findActivity.class);
         startActivity(intent);
+
     }
 
-    public void profile (View view){
+    public void profile(View view) {
         Intent intent = new Intent(this, myProfile.class);
         startActivity(intent);
     }
 
-    public void info (View view){
+    public void info(View view) {
         Intent intent = new Intent(this, appInfo.class);
         startActivity(intent);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Helper methods, need peavad olema igas klassis kus tahame infot küsida kasutaja kohta või seda muuta.
+
+
+    public void addXP(int amount){
+        writeFile(getName()+"\n"+(getXP()+amount));
+    }
+
+    public String getName() {
+        String in = readFile();
+        return in.split("\n")[0];
+    }
+    public int getXP() {
+        String in = readFile();
+        return Integer.parseInt(in.split("\n")[1]);
+    }
+
+    public void writeFile(String info) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("andmed.txt", MODE_PRIVATE);
+            fileOutputStream.write(info.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+        }
+    }
+
+
+    public String readFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput("andmed.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String lines;
+            while ((lines = bufferedReader.readLine()) != null) {
+                stringBuffer.append(lines + "\n");
+            }
+            return stringBuffer.toString();
+        } catch (Exception e) {
+        }
+        return null;
     }
 }
--- app/src/main/java/com/example/expe/myProfile.java
+++ app/src/main/java/com/example/expe/myProfile.java
@@ -1,13 +1,75 @@
 package com.example.expe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class myProfile extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
+        TextView txt = (TextView)findViewById(R.id.minuinfo);
+        txt.setText(readFile());
+    }
+
+    public void lisaVajutusel(View view){
+        addXP(30);
+        TextView txt = (TextView)findViewById(R.id.minuinfo);
+        txt.setText(readFile());
+    }
+
+
+
+
+
+    //Helper methods, need peavad olema igas klassis kus tahame infot küsida kasutaja kohta või seda muuta.
+
+    public void addXP(int amount){
+        writeFile(getName()+"\n"+(getXP()+amount));
+    }
+
+    public String getName() {
+        String in = readFile();
+        return in.split("\n")[0];
+    }
+    public int getXP() {
+        String in = readFile();
+        return Integer.parseInt(in.split("\n")[1]);
+    }
+
+    public void writeFile(String info) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("andmed.txt", MODE_PRIVATE);
+            fileOutputStream.write(info.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+        }
+    }
+
+
+    public String readFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput("andmed.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String lines;
+            while ((lines = bufferedReader.readLine()) != null) {
+                stringBuffer.append(lines + "\n");
+            }
+            return stringBuffer.toString();
+        } catch (Exception e) {
+        }
+        return null;
     }
 }
