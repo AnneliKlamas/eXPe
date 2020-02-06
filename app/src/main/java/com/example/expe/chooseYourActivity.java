4ce266cb183340fcd2b12352fee5ff28c592c18d
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2020-02-06 10:33:27 +0200

    added randomness to activity select

--- app/src/main/java/com/example/expe/chooseYourActivity.java
+++ app/src/main/java/com/example/expe/chooseYourActivity.java
@@ -20,9 +20,12 @@ public class chooseYourActivity extends AppCompatActivity {
     private boolean kasOlenÜksi;
     private Tegevus t1 = new Tegevus("Billiard", "You can play billiard there and there.", "The first coin-operated billiard table was patented in 1903.", false, 10);
     private Tegevus t2 = new Tegevus("Botanical garden", "Visit the botanical garden.", "Kew Gardens is the world's largest collection of living plants situated in London", true, 8);
-    private Tegevus[] arr = {t1, t2};
+    private Tegevus t3 = new Tegevus("Billiard2", "You can play billiard there and there.", "The first coin-operated billiard table was patented in 1903.", false, 10);
+    private Tegevus t4 = new Tegevus("Botanical garden2", "Visit the botanical garden.", "Kew Gardens is the world's largest collection of living plants situated in London", true, 8);
+    private Tegevus[] arr = {t1, t2,t3, t4};
     private Button bt1;
     private Button bt2;
+    private String abi = null;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,34 +40,58 @@ public class chooseYourActivity extends AppCompatActivity {
         xpväli.setText(String.valueOf(getXP()));
         elud.setText(String.valueOf(getElud()));
 
-        bt1 = (Button)findViewById(R.id.btt1);
-        bt1.setText(t1.getTitle());
+        bt1 = (Button) findViewById(R.id.btt1);
+        bt1.setText(annaSuvaliseTegevusePealkiri(kasOlenÜksi));
 
-        bt2 = (Button)findViewById(R.id.btt2);
-        bt2.setText(t2.getTitle());
+        bt2 = (Button) findViewById(R.id.btt2);
+        bt2.setText(annaSuvaliseTegevusePealkiri(kasOlenÜksi));
     }
 
-    public void pressedOption1(View view){
+    public void pressedOption1(View view) {
         writeMission(leiaKlikitud(bt1.getText().toString()));
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void pressedOption2(View view){
+    public void pressedOption2(View view) {
         writeMission(leiaKlikitud(bt2.getText().toString()));
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void pressedPass(View view){
+    public void pressedPass(View view) {
         eemaldaElu();
         Intent intent = new Intent(this, Feedback.class);
         startActivity(intent);
     }
 
-    public void eemaldaElu(){
-        writeProfile(getName()+"\n"+getXP() + "\n" + (getElud()-1));
+    public String annaSuvaliseTegevusePealkiri(boolean kasÜksiTegevus) {
+        List<Tegevus> valikud = new ArrayList<>();
+        for (Tegevus tegevus : arr) {
+            if (tegevus.isAloneActivity() == kasÜksiTegevus)
+                valikud.add(tegevus);
+        }
+        String pealkiri = "";
+        while (true) {
+            String t = valikud.get(juhuslik(0, valikud.size() - 1)).getTitle();
+            if (!t.equals(abi)) {
+                pealkiri = t;
+                abi = t;
+                break;
+            }
+        }
+        return pealkiri;
     }
+
+    public static int juhuslik(int a, int b) {
+        double x = Math.random() * (b - a + 1) + a;
+        return (int) x;
+    }
+
+    public void eemaldaElu() {
+        writeProfile(getName() + "\n" + getXP() + "\n" + (getElud() - 1));
+    }
+
     public void writeProfile(String info) {
         try {
             FileOutputStream fileOutputStream = openFileOutput("andmed.txt", MODE_PRIVATE);
@@ -74,7 +101,7 @@ public class chooseYourActivity extends AppCompatActivity {
         }
     }
 
-    public Tegevus leiaKlikitud(String klikitud){
+    public Tegevus leiaKlikitud(String klikitud) {
         for (Tegevus tegevus : arr) {
             if (tegevus.getTitle().equals(klikitud))
                 return tegevus;
@@ -109,6 +136,7 @@ public class chooseYourActivity extends AppCompatActivity {
         String in = readProfile();
         return in.split("\n")[0];
     }
+
     public int getXP() {
         String in = readProfile();
         return Integer.parseInt(in.split("\n")[1]);
@@ -117,11 +145,12 @@ public class chooseYourActivity extends AppCompatActivity {
 
     public void writeMission(Tegevus t) {
         try {
-            String info = t.getTitle()+"\n"+t.getDescription()+"\n"+t.getFunFact()+"\n"+t.getXP();
+            String info = t.getTitle() + "\n" + t.getDescription() + "\n" + t.getFunFact() + "\n" + t.getXP();
             FileOutputStream fileOutputStream = openFileOutput("missioon.txt", MODE_PRIVATE);
             fileOutputStream.write(info.getBytes());
             fileOutputStream.close();
         } catch (Exception e) {
         }
     }
+
 }
